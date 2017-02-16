042e1954ad0bb5e573da7e4e75a4c55a7c66db5a
johnTjohansen <jtjohansen@ca.rr.com>
2017-02-16 15:54:48 -0800

    commit point. still some game and layout issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,8 @@
   "BROCCOLI", "TOMATO", "LEMON", "KETCHUP", "MUSTARD",
   "MAYONNAISE", "YOGURT", "AVOCADO", "HUMMUS", "SALSA"];
   var numWins = 0;
+  var totGames = 0;
   var turnsRem = 8;
-  var guessedLet = [];
   var wrongLet = [];
   var numBlanks = 0;
   var currentWord = [];
@@ -15,138 +15,104 @@
 
   startGame();
   document.onkeyup = function(event) {
-  var guess = String.fromCharCode(event.keyCode).toUpperCase();
-  guessCheck(guess);
-  roundComplete();
+    var guess = String.fromCharCode(event.keyCode).toUpperCase();
+    guessCheck(guess);
+    roundComplete();
+  };
 
 
-function guessCheck(g) {
-/*	Check player's guess to see if letter is in the currentWord.
-	Replace the guess into the word layout - overlaying underscores
-	Or add guess to wrong guess list and decrease turns remaining.*/
-/*    validateGuess(guess);*/ 
-    var found = false;
-
-    for (var i = 0; i < currentWord.length; i++) {
-        if (currentWord[i] === g) {
-            found = true;
-        }
-    }
-
-	if (found) {
-		for (var i = 0; i < currentWord.length; i++) {
-			if (currentWord[i] === g) {
-				progressWord[i] = g;
-			}
-		    else {
-		      turnsRem--;
-			  wrongLet.push = g;
-			}  
-		}
-	}
-}
-
-function = startGame() {
+function startGame() {
 /* Pick a word and setup the current word line with '_' placeholders */
 
   turnsRem = 8;
   progressWord = [];
   wrongLet = [];	
 
+  console.log("in startGame function");
+
   /* choose a word from list*/
   currentWord = answers[Math.floor(Math.random() * answers.length)];
   /* set up answer underlines */
   for (var i = 0; i < currentWord.length; i++) {
-       progressWord.push = "_";
-  }
+       progressWord.push("_");
+  };
+  console.log(currentWord);
+  console.log(progressWord);
 
   document.getElementById('guessesLeft').innerHTML = turnsRem;
-  document.getElementById('currWord').innerHTML = progressWord.join;
+  document.getElementById('currWord').innerHTML = progressWord.join(" ");
+  document.getElementById('triedLetters').innerHTML = wrongLet;
 
-}
+/* end function */
+};
 
-/*function startGame() {
-*//* class example
-1. select word at random
-2. want to break up that random word into letters and replace them with underscores
-3. want to add those underscores to the HTML
-4. initialize maxGuesses and wrongGuesses and progressWord
-*/
-/*  turnsRem = 10;
-  progressWord = [];
-  wrongLet = [];
+function guessCheck(g) {
+/*	Check player's guess to see if letter is in the currentWord.
+	Replace the guess into the word layout - overlaying underscores
+	Or add guess to wrong guess list and decrease turns remaining.*/
+    console.log("in guessCheck function");
 
-  currentWord = answers[Math.floor(Math.random() * answers.length)];
-*/  /* split converts string to array */
-/*  lettersInChosenWord = currentWord.split();
-  numBlanks = lettersInChosenWord.length;
-*/
-/*  for(var i = 0; i < currentWord.length; i++) {
-  	progressWord.push("_");
-  }
+    var found = false;
+    var repeat = false;
 
-  document.getElementById('guessesLeft').innerHTML = turnsRem;
-  /* .join gets rid of commas within array - converts to string*/
-/*  document.getElementById('currWord').innerHTML = progressWord.join;
-} */
-
-/*function checkLetters(letter) {*/
-/* class example
-1. compare the guessed letter to match letters in word
-2. conditional statement to determine if the guess is in the word do this else do that
-3. if guess is wrong, decrease numGuesses by 1
-*/
-/*
-	var letterInWord = false;
-
-	for(var i = 0; i < numBlanks; i++) {
-		if(currentWord[i] === letter) {
-			letterInWord = true;
-		}
-	}
-	if (letterInWord) {
-		for (var i = 0; i < numBlanks; i++) {
-			if (currentWord[i] === letter) {
-				progressWord[i] = letter;
-			}
-		} else
+/*	Check player's guess to see if letter is in the currentWord.*/
+    for (var i = 0; i < currentWord.length; i++) {
+        if (currentWord[i] === g) {
+            found = true;
+        }
+    };
+/* Check player's guess to see if it's already guessed */
+    for (var i = 0; i < wrongLet.length; i++) {
+        if (wrongLet[i] === g) {
+            repeat = true;
+        }
+    };
+    console.log(repeat);
+/*  If the guess is a new letter and is in the word, replace it into
+    the screen word layout - overlaying underscores.  If not in word
+    decrease turnsRemaining and add guess to wrongLetter array. */
+    if (repeat !== true) { 
+	    if (found) {
+			for (var i = 0; i < currentWord.length; i++) {
+				if (currentWord[i] === g) {
+					progressWord[i] = g;
+				};
+			};
+			console.log("hitting the trigger" + progressWord)
+			document.getElementById('currWord').innerHTML = progressWord.join(" ");
+		}	
+		else {
 			turnsRem--;
-			wrongLet.push(letter);
-	}
-}*/
+			wrongLet.push(g);
+		};
+	};
+/* end function */
+};
 
 function roundComplete() {
-/* 
-1. update the HTML withe letters that are in the word
-2. update the HTML with guess we have left
-3. update the HTML to show the wrong guesses
-4. determine if game won or not
-*/
+/* 1. update the HTML and determine if game over */
+  console.log("in roundComplete function");
+
 	document.getElementById('currWord').innerHTML = progressWord.join(" ");
-	document.getElementById('guessesLeft').innerHTML = numGuesses.join(" ");
+	document.getElementById('guessesLeft').innerHTML = turnsRem;
 	document.getElementById('triedLetters').innerHTML = wrongLet.join(" ");
-	document.getElementById('gamesWon').innerHTML = numWins;
-/*	if progressWord.indexOf(letter >= 1) {
-		console.log(letter);
-	}*/
-	if (progressWord.join(" ") === currentWord.join(" ")) {
+
+/* if word has been completely guessed, process win and setup new game */	
+	if (progressWord.join("") === currentWord) {
 		numWins++;
-		alert("you win");
+		totGames++;
 		document.getElementById("gamesWon").innerHTML = numWins;
+		document.getElementById("totGames").innerHTML = totGames;
+		alert("You win!");
 		startGame();
-	} else {
-		document.getElementById("you lost").innerHTML = numLosses;
+	};
+
+/* when turnsRemaining counter runs out, process loss and setup new game */	
+	if (turnsRem === 0) {
+		totGames++;
+		document.getElementById("totGames").innerHTML = totGames;
+	    alert("Sorry, you lose");
 		startGame();
-	}
-
-}
-
-/*startGame();
-document.onkeyup = function(event) {
-	var guess = String.fromCharCode(event.keyCode).toUpperCase();
-	checkLetters(guess);
-	roundComplete();*/
-/* 
-
-*/
-}
+	};
+/* end function */
+};
